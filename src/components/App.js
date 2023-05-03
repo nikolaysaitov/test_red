25cac43394f9873033fb0d7210022e4ac0131b6c
nikolay <saitov@impermebel.ru>
2023-05-03 15:02:46 +0300

    redux

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import Counter from "./Counter";
+import React from 'react'
+
+const App = () => {
+  return (
+    <Counter/>
+  )
+}
+
+export default App
---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {inc, dec, rnd} from "../actions";
+
+
+
+const Counter = () => {
+
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="jumbotron">
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(dec())} className="btn btn-primary">
+        DEC
+      </button>
+      <button onClick={() => dispatch(inc())} className="btn btn-primary">
+        INC
+      </button>
+      <button onClick={() => dispatch(rnd())} className="btn btn-primary">
+        RND
+      </button>
+    </div>
+  );
+};
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.value,
+//   };
+// };
+
+// export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
